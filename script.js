5e0b5fba7380834f15ba7fbf17d6f49a22ab004c
Aman Raj <101209058+henryaman016@users.noreply.github.com>
2022-09-21 16:41:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const count=document.getElementById("count");
+const add=document.getElementById("add");
+const sub=document.getElementById("sub");
+const reset=document.getElementById("reset");
+
+add.addEventListener("click",()=>{
+    count.innerHTML++;
+    applycolor()
+})
+
+
+sub.addEventListener("click",()=>{
+    count.innerHTML--;
+    applycolor()
+})
+
+
+reset.addEventListener("click",()=>{
+    count.innerHTML=0;
+    applycolor()
+})
+
+function applycolor(){
+    if(count.innerHTML > 0){
+        count.style.color="green"
+    }else if(count.innerHTML < 0){
+        count.style.color='orangered';
+    }else{
+        count.style.color="black";
+    }
+}
